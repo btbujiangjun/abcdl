7530762157ea243254da1d815dcf7c5a13fe33a1
baidu <baidu@MacBook-Pro.lan>
2017-07-31 22:50:41 +0800

    initialized

--- include/algebra/Matrix.h
+++ include/algebra/Matrix.h
@@ -20,38 +20,38 @@ template<class T>
 class Matrix{
 public:
     Matrix();
-    Matrix(const std::size_t rows, const std::size_t cols);
+    Matrix(const size_t rows, const size_t cols);
     Matrix(const T& value,
-           const std::size_t rows,
-           const std::size_t cols);
+           const size_t rows,
+           const size_t cols);
     Matrix(const T* data,
-           const std::size_t rows,
-           const std::size_t cols);
+           const size_t rows,
+           const size_t cols);
     ~Matrix();
 
-    inline std::size_t rows() const { return _rows;}
-    inline std::size_t cols() const { return _cols;}
-    inline std::size_t get_size() const { return _rows * _cols;}
+    inline size_t rows() const { return _rows;}
+    inline size_t cols() const { return _cols;}
+    inline size_t get_size() const { return _rows * _cols;}
 
     inline T* data() const { return _data; }
-    inline T& get_data(const std::size_t idx) const{
+    inline T& get_data(const size_t idx) const{
 		return _data[idx];
 	}
-    inline T& get_data(const std::size_t row_id, const std::size_t col_id) const{
+    inline T& get_data(const size_t row_id, const size_t col_id) const{
 		return _data[row_id * _cols + col_id];
 	}
 
-    inline void set_data(const T& value, const std::size_t idx){
+    inline void set_data(const T& value, const size_t idx){
 		_data[idx] = value;
 	}
     inline void set_data(const T& value,
-                         const std::size_t row_id,
-                         const std::size_t col_id){
+                         const size_t row_id,
+                         const size_t col_id){
 		_data[row_id * _cols + col_id] = value;
 	}
     inline void set_data(const T* data,
-                         const std::size_t rows,
-                         const std::size_t cols){
+                         const size_t rows,
+                         const size_t cols){
         if(_rows * _cols != rows * cols){
             if(_data != nullptr){
                 delete[] _data;
@@ -67,38 +67,38 @@ public:
 	}
 
     void set_shallow_data(T* data,
-                          const std::size_t rows,
-                          const std::size_t cols);
+                          const size_t rows,
+                          const size_t cols);
     
-    Matrix<T>* get_row(const std::size_t row_id, const std::size_t row_size = 1);
+    Matrix<T>* get_row(const size_t row_id, const size_t row_size = 1);
     void get_row(Matrix<T>& mat,
-				 const std::size_t row_id,
-				 const std::size_t row_size = 1);
+				 const size_t row_id,
+				 const size_t row_size = 1);
     void set_row(const Matrix<T>& mat);
-    void set_row(const std::size_t row_id, const Matrix<T>& mat);
+    void set_row(const size_t row_id, const Matrix<T>& mat);
     void insert_row(const Matrix<T>& mat);
-    void insert_row(const std::size_t row_id, const Matrix<T>& mat);
+    void insert_row(const size_t row_id, const Matrix<T>& mat);
 
-    Matrix<T>* get_col(const std::size_t col_id, const std::size_t col_size = 1);
+    Matrix<T>* get_col(const size_t col_id, const size_t col_size = 1);
 	void get_col(Matrix<T>& mat,
-				 const std::size_t col_id,
-				 const std::size_t col_size = 1);
+				 const size_t col_id,
+				 const size_t col_size = 1);
     void set_col(const Matrix<T>& mat);
-    void set_col(const std::size_t col_id, const Matrix<T>& mat);
+    void set_col(const size_t col_id, const Matrix<T>& mat);
     void insert_col(const Matrix<T>& mat);
-    void insert_col(const std::size_t col_id, const Matrix<T>& mat);
+    void insert_col(const size_t col_id, const Matrix<T>& mat);
 
     Matrix<T>* clone() const;
     void clone(Matrix<T>& mat) const;
 
     void reset(const T& value = 0);
     void reset(const T& value,
-               const std::size_t rows,
-               const std::size_t cols);
+               const size_t rows,
+               const size_t cols);
 
     void transpose();
 
-    inline bool reshape(std::size_t row, std::size_t col){
+    inline bool reshape(size_t row, size_t col){
         if(row * col == _rows * _cols){
             _rows = row;
             _cols = col;
@@ -110,7 +110,7 @@ public:
     void display(const std::string& split="\t");
 
     //operator
-	T& operator [] (const std::size_t idx) const;
+	T& operator [] (const size_t idx) const;
 	bool operator == (const Matrix<T>& mat) const;
 
     Matrix<T>& operator = (const T& value);
@@ -153,6 +153,8 @@ public:
 	T min() const;
     T sum() const;
     real mean() const;
+    bool inverse(Matrix<real>& mat);
+
 //    bool det(T* result);
 /*
 
@@ -160,32 +162,32 @@ public:
     void x_sum();
     void y_sum();
 
-    Matrix<int>* argmax(const std::size_t axis);
-    std::size_t argmax(const std::size_t id, const std::size_t axis);
+    Matrix<int>* argmax(const size_t axis);
+    size_t argmax(const size_t id, const size_t axis);
 
 	bool isnan();
 	bool isinf();
 
-    virtual bool swap(const std::size_t a_row,
-                      const std::size_t a_col,
-                      const std::size_t b_row,
-                      const std::size_t b_col) = 0;
-    virtual bool swap_row(const std::size_t a, const std::size_t b) = 0;
-    virtual bool swap_col(const std::size_t a, const std::size_t b) = 0;
+    virtual bool swap(const size_t a_row,
+                      const size_t a_col,
+                      const size_t b_row,
+                      const size_t b_col) = 0;
+    virtual bool swap_row(const size_t a, const size_t b) = 0;
+    virtual bool swap_col(const size_t a, const size_t b) = 0;
 
     virtual Matrix<T>* transpose() = 0;
 
 
-    void expand(std::size_t row_dim, std::size_t col_dim);
+    void expand(size_t row_dim, size_t col_dim);
 
     //shape is full or valid, default full.
     bool convn(ccma::algebra::Matrix<T>* kernal,
-               std::size_t stride = 1,
+               size_t stride = 1,
                std::string shape = "full");
 
     
     //convert dim 1 is row dim, 2 is col dim
-    void flipdim(std::size_t dim = 1);
+    void flipdim(size_t dim = 1);
     void flip180();
 
     virtual void add_x0() = 0;
@@ -193,10 +195,10 @@ public:
 
     virtual bool det(T* result) = 0;
 
-    virtual real mean(std::size_t col) = 0;
+    virtual real mean(size_t col) = 0;
 
     virtual real var() = 0;
-    virtual real var(std::size_t col) = 0;
+    virtual real var(size_t col) = 0;
 
     virtual bool inverse(Matrix<real>* result) = 0;
 */
@@ -206,10 +208,14 @@ private:
         return _rows == mat.rows() && _cols == mat.cols();
     }
 private:
-    std::size_t _rows;
-    std::size_t _cols;
+    size_t _rows;
+    size_t _cols;
     T*   _data;
 };//class Matrix
+
+
+typedef Matrix<real> Mat;
+typedef Matrix<int> IMat;
 
 }//namespace algebra
 }//namespace abcdl
