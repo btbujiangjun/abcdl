f82e97e379e793ace9c37292f405a629b2129a59
btbujiangjun <jiangjun4@sina.com>
2017-08-22 16:36:39 +0800

    initialized

--- include/dnn/ActivateFunc.h
+++ include/dnn/ActivateFunc.h
@@ -17,27 +17,27 @@ namespace dnn{
 class ActivateFunc{
 public:
     virtual ~ActivateFunc() = default;
-    virtual void activate(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& z_mat) = 0;
-    virtual void derivative(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& activate_mat) = 0;
+    virtual void activate(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& z_mat) = 0;
+    virtual void derivative(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& activate_mat) = 0;
 protected:
     abcdl::algebra::MatrixHelper<real> helper;
 };//class ActivateFunc
 
 class SigmoidActivateFunc : public ActivateFunc{
 public:
-    void activate(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& z_mat){
+    void activate(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& z_mat){
         helper.sigmoid(mat, z_mat);
     }
-    void derivative(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& activate_mat){
+    void derivative(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& activate_mat){
         helper.sigmoid_derivative(mat, activate_mat);
     }
 };//class SigmoidActivateFunc
 
 class ReluActivateFunc : public ActivateFunc{
-    void activate(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& z_mat){
+    void activate(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& z_mat){
         helper.relu(mat, z_mat);
     }
-    void derivative(abcdl::algebra::Mat mat, const abcdl::algebra::Mat& activate_mat){
+    void derivative(abcdl::algebra::Mat& mat, const abcdl::algebra::Mat& activate_mat){
     }
 };//class ReluActivateFunc
 
