f1e8c0e374c817cc7e3b7c43e24354e00a294957
btbujiangjun <jiangjun4@sina.com>
2017-08-24 18:08:03 +0800

    initialized

--- example/dnn.cpp
+++ example/dnn.cpp
@@ -30,16 +30,16 @@ int main(int argc, char** argv){
     abcdl::utils::MnistHelper<real> helper;
     
     abcdl::algebra::Mat train_data;
-    helper.read_image("data/mnist/train-images-idx3-ubyte", &train_data, 1000);
+    helper.read_image("data/mnist/train-images-idx3-ubyte", &train_data, -1);
 
     abcdl::algebra::Mat train_label;
-    helper.read_vec_label("data/mnist/train-labels-idx1-ubyte", &train_label, 1000);
+    helper.read_vec_label("data/mnist/train-labels-idx1-ubyte", &train_label, -1);
 
     abcdl::algebra::Mat test_data;
-    helper.read_image("data/mnist/t10k-images-idx3-ubyte", &test_data, 100);
+    helper.read_image("data/mnist/t10k-images-idx3-ubyte", &test_data, -1);
 
     abcdl::algebra::Mat test_label;
-    helper.read_vec_label("data/mnist/t10k-labels-idx1-ubyte", &test_label, 100);
+    helper.read_vec_label("data/mnist/t10k-labels-idx1-ubyte", &test_label, -1);
 
     dnn.train(train_data, train_label, test_data, test_label);
 
