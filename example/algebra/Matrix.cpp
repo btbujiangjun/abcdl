3ba0efd06054c9e8dfe94579a4400cc8a2930ee2
btbujiangjun <jiangjun4@sina.com>
2017-08-24 15:50:39 +0800

    initialized

--- example/algebra/Matrix.cpp
+++ example/algebra/Matrix.cpp
@@ -15,15 +15,26 @@ using abcdl::algebra::Mat;
 int main(int argc,char** argv){
     abcdl::utils::log::set_min_log_level(abcdl::utils::log::INFO);
     abcdl::utils::log::initialize_log(argc, argv);
-    real d1[6] = {1,2,3,4,5,6};
-    real d2[6] = {1,2,3,4,5,6};
+    real d1[6] = {1,2,-3,4,5,6};
     Mat m1(d1, 2, 3);
-    Mat m2(d2, 3, 2);
-
     m1.display();
-    m2.display();
+
+    auto m3 = m1 * m1;
+    m3.display();
+
+    m1 += m1;
+    m1.display();
+
+    m1.sigmoid();
+    m1.display();
 
     abcdl::algebra::MatrixHelper<real> helper;
-    auto m3 = helper.dot(m1, m2);
-    m3.display();
+    Mat m2;
+    helper.sigmoid_derivative(m2, m1);
+    m2.display();
+
+    m1 *= -1;
+    m1.display();
+    auto m4 = m2 * m1;
+    m4.display();
 }
