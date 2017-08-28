f6af8eda1aca4bd065b4fd2db0e84dd46c337e63
btbujiangjun <jiangjun4@sina.com>
2017-08-28 19:51:07 +0800

    initialized

--- include/cnn/Pool.h
+++ include/cnn/Pool.h
@@ -17,37 +17,37 @@ class Pooling{
 public:
     virtual ~Pooling(){}
     virtual void pool(abcdl::algebra::Mat& pool,
+                      const abcdl::algebra::Mat& mat,
                       const size_t rows,
                       const size_t cols,
-                      const size_t scale,
-                      const abcdl::algebra::Mat& mat);
+                      const size_t scale);
 };//class Pooling
 
 class MeanPooling : public Pooling{
 public:
     void pool(abcdl::algebra::Mat& pool,
+              const abcdl::algebra::Mat& mat,
               const size_t rows,
               const size_t cols,
-              const size_t scale,
-              const abcdl::algebra::Mat& mat);
+              const size_t scale);
 };//class MeanPooling
 
 class MaxPooling : public Pooling{
 public:
     void pool(abcdl::algebra::Mat& pool,
+              const abcdl::algebra::Mat& mat,
               const size_t rows,
               const size_t cols,
-              const size_t scale,
-              const abcdl::algebra::Mat& mat);
+              const size_t scale);
 };//class MaxPooling
 
 class L2Pooling : public Pooling{
 public:
     void pool(abcdl::algebra::Mat& pool,
+              const abcdl::algebra::Mat& mat,
               const size_t rows,
               const size_t cols,
-              const size_t scale,
-              const abcdl::algebra::Mat& mat);
+              const size_t scale);
 };//class L2Pooling
 
 }//namespace cnn
