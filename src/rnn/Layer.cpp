13e1b286a40d2782d7da532a46cd963f08db0119
baidu <baidu@MacBook-Pro.lan>
2017-09-06 22:15:50 +0800

    initialize

--- src/rnn/Layer.cpp
+++ src/rnn/Layer.cpp
@@ -19,7 +19,6 @@ void Layer::farward(const abcdl::algebra::Mat& train_seq_data,
 	
 	size_t seq_rows = train_seq_data.rows();
 	size_t seq_cols = train_seq_data.cols();
-	
 	state.reset(0, seq_rows, _hidden_dim);
 	activation.reset(0, seq_rows, seq_cols);
 
@@ -31,9 +30,9 @@ void Layer::farward(const abcdl::algebra::Mat& train_seq_data,
 			s_t = (helper.dot(weight, train_seq_data.get_row(t).Ts()) + helper.dot(pre_weight, state.get_row(t - 1).Ts())).tanh().Ts();
 		}else{
 			s_t = helper.dot(weight, train_seq_data.get_row(t).Ts()).tanh().Ts();
-		}	
-		state.set_row(t, s_t);
+		}
 
+		state.set_row(t, s_t);
 		activation.set_row(t, helper.dot(act_weight, s_t).softmax().Ts())	
 	}
 }
@@ -49,6 +48,10 @@ void Layer::backward(const abcdl::algebra::Mat& train_seq_data,
 
     auto now = []{return std::chrono::system_clock::now();};
     auto start_time = now();
+	
+	derivate_weight.reset(0, weight.rows(), weight.cols());
+	derivate_pre_weight.reset(0, pre_weight.rows(), pre_weight.cols());
+	derivate_act_weight.reset(0, act_weight.rows(), act_weight.cols());
 
 	farward(train_seq_data, weight, pre_weight, act_weight, state, derivate_output);
 	derivate_output.subtract(train_seq_label);
@@ -56,26 +59,27 @@ void Layer::backward(const abcdl::algebra::Mat& train_seq_data,
 	size_t seq_size = train_seq_data.rows();
 
 	for(sizt_t t = seq_size - 1; t >= 0 ; t--){
-		auto derivate_output_tt = derivate_output.get_row(t).Ts();
+		auto derivate_output_t = derivate_output.get_row(t).Ts();
+
         //update derivate_act_weight
-		dervate_act_weight += derivate_output_tt.outer(state.get_row(t).Ts());
+		dervate_act_weight += helper.outer(derivate_output_t, state.get_row(t).Ts());;
 
         //calc derivate_t
-		auto derivate_t = helper.dot(act_weight.Ts(), derivate_output_tt) * state.get_row(t).Ts().sigmoid_derivative();
+		auto derivate_t = helper.dot(act_weight.Ts(), derivate_output_t) * state.get_row(t).Ts().sigmoid_derivative();
 
 		//back_propagation steps
 		for(size_t step = 0; step < _bptt_truncate && (int)step <= t; step++){
-			int bptt_step = t - step;
+			size_t bptt_step = t - step;
 
             if(debug){
-    			printf("Backpropagation step t=%d bptt step=%d\n", t, bptt_step);
+    			printf("Backpropagation step t=%ld bptt step=%ld\n", t, bptt_step);
             }
 
 			abcdl::algebra::Mat derivate_state_t;
 			if(bptt_step > 0){
 				state.get_row(&derivate_state_t, bptt_step -1);
 			}else{
-				derivate_state_t.reset(0, state.get_cols(), 1);
+				derivate_state_t.reset(0, state.cols(), 1);
 			}
 
             //update derivate_pre_weight
@@ -84,36 +88,17 @@ void Layer::backward(const abcdl::algebra::Mat& train_seq_data,
             //update derivate_weight
             auto derivate_weight_t = helper.dot(derivate_weight, train_seq_data.get_row(bptt_step).Ts()) + derivate_t;
 
-            size_t idx = train_data_t.argmax(0, 1);
-            derivate_weight.get_col_data(idx, derivate_weight_t_c);
-            derivate_weight_t_c.add(derivate_weight_t);
-            derivate_weight.set_col_data(idx, derivate_weight_t_c);
+            size_t idx = train_data_t.argmax(0, abcdl::algebra::Axis_type::ROW);
+            derivate_weight.set_col(idx, derivate_weight.get_col(idx) + derivate_weight_t);
 
 			//update delta
 			if(bptt_step > 0){
-				derivate_state_t.multiply(derivate_state_t);
-				derivate_state_t.multiply(-1);
-				derivate_state_t.add(1);
-
-				pre_weight.clone(derivate_pre_weight_t);
-				derivate_pre_weight_t.transpose().dot(derivate_t);
-				derivate_pre_weight_t.multiply(derivate_state_t);
-				derivate_pre_weight_t.clone(derivate_t);
+				helper.sigmoid_derivative(derivate_state_t, derivate_state_t);
+				derivate_t = pre_weight.Ts().dot(derivate_t) * derivate_state_t;
 			}
 		}
 	}
-
-	delete derivate_output;
-	delete derivate_output_t;
-    delete derivate_weight_t;
-    delete derivate_weight_t_c;
-	delete derivate_t;
-	delete derivate_pre_weight_t;
-	delete derivate_state_t;
-	delete train_data_t;
-
-    auto end_time = now();
-    printf("thread[%lu][%ld-%ld][%ld].\n", std::this_thread::get_id(), start_time, end_time, std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count());
+    printf("thread[%lu][%ld-%ld][%ld].\n", std::this_thread::get_id(), start_time, end_time, std::chrono::duration_cast<std::chrono::milliseconds>(now() - start_time).count());
 }
 
 
