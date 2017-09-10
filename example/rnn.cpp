30ec21101e98ea75d445b81aa630f789ec111bfc
baidu <baidu@MacBook-Pro.lan>
2017-09-10 15:15:36 +0800

    initialize

---
+++ example/rnn.cpp
@@ -0,0 +1,36 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2017-06-01 11:22
+* Last modified: 2017-06-01 11:22
+* Filename: TestRNN.cpp
+* Description: 
+**********************************************/
+
+#include "utils/RNNHelper.h"
+#include "rnn/RNN.h"
+
+int main(int argc, char** argv){
+	std::map<std::string, size_t> map;
+	abcdl::utils::RNNHelper helper(8000);
+	helper.read_word2index("data/word_to_index", map);
+
+	std::vector<abcdl::algebra::Mat*> data_seq_data;
+	std::vector<abcdl::algebra::Mat*> data_seq_label;
+	helper.read_seqdata("data/train_seq_data", &data_seq_data, "data/train_seq_label", &data_seq_label, 1000);
+
+    printf("Start training....\n");
+
+    abcdl::rnn::RNN rnn(8000, 100);
+    rnn.train(data_seq_data, data_seq_label);
+
+	for(auto&& d : data_seq_data){
+		delete d;
+	}
+	data_seq_data.clear();
+	
+	for(auto&& d : data_seq_label){
+		delete d;
+	}
+	data_seq_label.clear();
+
+}
