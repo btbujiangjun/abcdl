63c84bd07868921ca590cbf29df5538783bed381
baidu <baidu@MacBook-Pro.lan>
2017-09-18 23:02:34 +0800

    initialize

--- example/rnn.cpp
+++ example/rnn.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv){
 	//std::map<std::string, size_t> map;
 	//helper.read_word2index("data/rnn/word_to_index", map);
 
-    const size_t sample_size = 10000;
+    const size_t sample_size = 1000;
 	std::vector<abcdl::algebra::Mat*> data_seq_data;
 	std::vector<abcdl::algebra::Mat*> data_seq_label;
 	if(!helper.read_seq_data("data/rnn/train_seq_data", data_seq_data, "data/rnn/train_seq_label", data_seq_label, sample_size)){
@@ -29,6 +29,10 @@ int main(int argc, char** argv){
     LOG(INFO) << "RNN Start training....";
 
     abcdl::rnn::RNN rnn(8000, 100);
+    rnn.set_epoch(50);
+    rnn.load_model("./data/rnn_seq.model");
+
+    LOG(INFO) << "training size:" << data_seq_data.size();
     rnn.train(data_seq_data, data_seq_label);
 
 	for(auto&& d : data_seq_data){
