a7b292c796446a4a25ecddb74303ad25beefab2b
btbujiangjun <jiangjun4@sina.com>
2017-09-20 15:46:42 +0800

    initialize

---
+++ example/fnn.cpp
@@ -0,0 +1,46 @@
+/***********************************************
+ * Author: Jun Jiang - jiangjun4@sina.com
+ * Create: 2017-08-19 21:06
+ * Last modified : 2017-08-19 21:06
+ * Filename      : fnn.cpp
+ * Description   : 
+ **********************************************/
+#include <vector>
+#include "fnn/FNN.h"
+#include "utils/Log.h"
+#include "utils/MnistHelper.h"
+
+int main(int argc, char** argv){
+    abcdl::utils::log::set_min_log_level(abcdl::utils::log::INFO);
+    abcdl::utils::log::initialize_log(argc, argv);
+
+    const std::string path = "./data/fnn.model";
+
+    abcdl::fnn::FNN fnn;
+    if(!fnn.load_model(path)){
+        std::vector<abcdl::fnn::Layer*> layers;
+        layers.push_back(new abcdl::fnn::InputLayer(784));
+        layers.push_back(new abcdl::fnn::FullConnLayer(784, 30, new abcdl::framework::SigmoidActivateFunc()));
+        layers.push_back(new abcdl::fnn::OutputLayer(30, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
+        fnn.set_layers(layers);
+    }
+
+    abcdl::utils::MnistHelper<real> helper;
+   
+    const int train_size = -1;
+    const int test_size = -1;
+
+    abcdl::algebra::Mat train_data;
+    helper.read_image("data/mnist/train-images-idx3-ubyte", &train_data, train_size);
+    abcdl::algebra::Mat train_label;
+    helper.read_vec_label("data/mnist/train-labels-idx1-ubyte", &train_label, train_size);
+
+    abcdl::algebra::Mat test_data;
+    helper.read_image("data/mnist/t10k-images-idx3-ubyte", &test_data, test_size);
+    abcdl::algebra::Mat test_label;
+    helper.read_vec_label("data/mnist/t10k-labels-idx1-ubyte", &test_label, test_size);
+
+    fnn.train(train_data, train_label, test_data, test_label);
+
+    fnn.write_model(path);
+}
