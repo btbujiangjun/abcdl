f8d23110581ea8e55d2b0c15641cd1162b7e6327
btbujiangjun <jiangjun4@sina.com>
2017-09-21 16:00:38 +0800

    remove useless code

--- include/algebra/MatrixHelper.h
+++ include/algebra/MatrixHelper.h
@@ -20,30 +20,30 @@ class MatrixHelper{
 public:
     Matrix<T> dot(const Matrix<T>& mat_a, const Matrix<T>& mat_b);
     void dot(Matrix<T>& mat,
-			 const Matrix<T>& mat_a,
-			 const Matrix<T>& mat_b);
+             const Matrix<T>& mat_a,
+             const Matrix<T>& mat_b);
     Matrix<T> outer(const Matrix<T>& mat_a, const Matrix<T>& mat_b);
     void outer(Matrix<T>& mat,
-			   const Matrix<T>& mat_a,
-			   const Matrix<T>& mat_b);
+               const Matrix<T>& mat_a,
+               const Matrix<T>& mat_b);
     void pow(Matrix<T>& mat,
-			 const Matrix<T>& mat_a,
-			 const T& exponent);
+             const Matrix<T>& mat_a,
+             const T& exponent);
     void log(Matrix<T>& mat,
-			 const Matrix<T>& mat_a);
+             const Matrix<T>& mat_a);
     void exp(Matrix<T>& mat,
-			 const Matrix<T>& mat_a);
+             const Matrix<T>& mat_a);
     void sigmoid(Matrix<T>& mat, const Matrix<T>& mat_a);
     void sigmoid_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void softmax(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void tanh(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void tanh_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void relu(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void relu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void leaky_relu(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void leaky_relu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void elu(Matrix<T>& mat, const Matrix<T>& mat_a);
-	void elu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void softmax(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void tanh(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void tanh_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void relu(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void relu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void leaky_relu(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void leaky_relu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void elu(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void elu_derivative(Matrix<T>& mat, const Matrix<T>& mat_a);
     void expand(Matrix<T>& result,
                 const Matrix<T>& mat,
                 const size_t row_dim,
@@ -55,7 +55,7 @@ public:
                const size_t stride,
                const Convn_type type = VALID);
 
-	void transpose(Matrix<T>& mat, const Matrix<T>& mat_a);
+    void transpose(Matrix<T>& mat, const Matrix<T>& mat_a);
 
     void zero_like(Matrix<T>& mat, const Matrix<T>& mat_a);
 
