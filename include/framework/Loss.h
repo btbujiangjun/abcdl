13d18e86c05519823c8092e11bf4ce1d6229cafe
btbujiangjun <jiangjun4@sina.com>
2017-09-28 15:47:42 +0800

    display train loss

---
+++ include/framework/Loss.h
@@ -0,0 +1,32 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2017-09-28 15:22
+* Last modified: 2017-09-28 15:22
+* Filename: Loss.h
+* Description: loss function
+**********************************************/
+#pragma once
+
+#include "algebra/Matrix.h"
+
+namespace abcdl{
+namespace framework{
+
+class Loss{
+public:
+    virtual ~Loss() = default;
+    virtual real loss(const abcdl::algebra::Mat& label,
+                      const abcdl::algebra::Mat& activate) = 0;
+};//class Loss
+
+class MSELoss : public Loss{
+public:
+    real loss(const abcdl::algebra::Mat& label,
+              const abcdl::algebra::Mat& activate){
+        auto diff_mat = label - activate;
+        return (diff_mat * diff_mat).sum();
+    }
+};//class MSELoss
+
+}//namespace framework
+}//namespace abcdl
