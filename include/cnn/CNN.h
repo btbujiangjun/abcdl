9cd788f5d8768f0be6da403063064657a8707ed1
btbujiangjun <jiangjun4@sina.com>
2017-09-30 16:49:41 +0800

    optimize matrix wrapper

--- include/cnn/CNN.h
+++ include/cnn/CNN.h
@@ -9,6 +9,7 @@
 
 #include <vector>
 #include "algebra/Matrix.h"
+#include "algebra/MatrixSet.h"
 #include "cnn/Layer.h"
 #include "utils/Log.h"
 #include "utils/ModelLoader.h"
@@ -29,21 +30,21 @@ public:
     void set_batch_size(const size_t batch_size){_batch_size = batch_size;}
     void set_layers(std::vector<abcdl::cnn::Layer*> layers);
     
-    void train(const abcdl::algebra::Mat& train_data,
-               const abcdl::algebra::Mat& train_label,
-               const abcdl::algebra::Mat& test_data,
-               const abcdl::algebra::Mat& test_label);
+    void train(const abcdl::algebra::MatSet& train_data,
+               const abcdl::algebra::MatSet& train_label,
+               const abcdl::algebra::MatSet& test_data,
+               const abcdl::algebra::MatSet& test_label);
     void predict(abcdl::algebra::Mat& result, const abcdl::algebra::Mat& predict_data);
 
 	bool load_model(const std::string& path){return true;}
 	bool write_model(const std::string& path){return true;}
 private:
-    size_t evaluate(const abcdl::algebra::Mat& data, const abcdl::algebra::Mat& label);
+    size_t evaluate(const abcdl::algebra::MatSet& data, const abcdl::algebra::MatSet& label);
     void forward(const abcdl::algebra::Mat& mat);
     void backward(const abcdl::algebra::Mat& mat);
     void update_gradient(const size_t batch_size, const real alpha);
 
-    bool check(const size_t size) const;
+    bool check(const size_t rows, const size_t cols) const;
 private:
 	size_t _epoch = 50;
     size_t _batch_size = 1;
