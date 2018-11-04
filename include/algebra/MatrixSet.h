032fb6baa8d748d31a2e1e7614c75dd609b9832e
baidu <baidu@MacBook-Pro.lan>
2018-11-04 23:18:23 +0800

    matrix set

--- include/algebra/MatrixSet.h
+++ include/algebra/MatrixSet.h
@@ -10,29 +10,31 @@
 #include "algebra/Matrix.h"
 #include <vector>
 
-using abcdl::algebra::Matrix;
-
 namespace abcdl{
 namespace algebra{
 
 template<class T>
-class MatrixSet : public std::vector<abcdl::algebra::Matrix<T>>{
+class MatrixSet{
 public:
-
     void push_back(abcdl::algebra::Matrix<T> mat){
         _matset.push_back(mat);
     }
 
+    abcdl::algebra::Matrix<T> operator [] (size_t idx) const{
+        return _matset[idx];
+    }
 
     inline const size_t size() const{
         return _matset.size();
     }
+
     inline const size_t rows() const{
-        if( _matset.size() == 0){
+        if( _matset.size() == 0 ){
             return 0;
         }
         return _matset[0].rows();
     }
+
     inline const size_t cols() const{
         if( _matset.size() == 0){
             return 0;
