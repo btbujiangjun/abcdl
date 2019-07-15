856a3b0d9a1c3e4cc186c35a450d0496f187d337
jiangjun <jiangjun@jiangjun.local>
2019-07-15 21:49:27 +0800

    relu demo

--- example/fnn.cpp
+++ example/fnn.cpp
@@ -17,18 +17,25 @@ int main(int argc, char** argv){
     const std::string path = "./data/fnn.model";
 
     abcdl::fnn::FNN fnn;
+    fnn.set_alpha(0.002);
+    fnn.set_epoch(100);
     if(!fnn.load_model(path)){
         std::vector<abcdl::fnn::Layer*> layers;
         layers.push_back(new abcdl::fnn::InputLayer(784));
-        layers.push_back(new abcdl::fnn::FullConnLayer(784, 30, new abcdl::framework::SigmoidActivateFunc()));
-        layers.push_back(new abcdl::fnn::OutputLayer(30, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
+        layers.push_back(new abcdl::fnn::FullConnLayer(784, 128, new abcdl::framework::ReluActivateFunc()));
+        layers.push_back(new abcdl::fnn::FullConnLayer(128, 128, new abcdl::framework::ReluActivateFunc()));
+        layers.push_back(new abcdl::fnn::FullConnLayer(128, 64, new abcdl::framework::ReluActivateFunc()));
+        layers.push_back(new abcdl::fnn::FullConnLayer(64, 32, new abcdl::framework::ReluActivateFunc()));
+        layers.push_back(new abcdl::fnn::FullConnLayer(32, 16, new abcdl::framework::ReluActivateFunc()));
+        layers.push_back(new abcdl::fnn::OutputLayer(16, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
         fnn.set_layers(layers);
     }
 
     const int train_size = -1;
     const int test_size = -1;
     
-    abcdl::utils::FashionMnistReader<real> helper("data");
+    //abcdl::utils::FashionMnistReader<real> helper("data");
+    abcdl::utils::MnistReader<real> helper("data");
     abcdl::algebra::Mat train_data;
 
     helper.read_train_image(&train_data, train_size);
