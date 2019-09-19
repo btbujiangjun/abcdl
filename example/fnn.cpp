fff999d77c551c51212e03d5fdf7be9cf1099d64
jiangjun <jiangjun@jiangjun.local>
2019-09-19 21:01:57 +0800

    demo

--- example/fnn.cpp
+++ example/fnn.cpp
@@ -17,19 +17,25 @@ int main(int argc, char** argv){
     const std::string path = "./data/fnn.model";
 
     abcdl::fnn::FNN fnn;
-    fnn.set_alpha(0.002);
-    fnn.set_epoch(100);
-    if(!fnn.load_model(path)){
-        std::vector<abcdl::fnn::Layer*> layers;
-        layers.push_back(new abcdl::fnn::InputLayer(784));
-        layers.push_back(new abcdl::fnn::FullConnLayer(784, 128, new abcdl::framework::ReluActivateFunc()));
-        layers.push_back(new abcdl::fnn::FullConnLayer(128, 128, new abcdl::framework::ReluActivateFunc()));
-        layers.push_back(new abcdl::fnn::FullConnLayer(128, 64, new abcdl::framework::ReluActivateFunc()));
-        layers.push_back(new abcdl::fnn::FullConnLayer(64, 32, new abcdl::framework::ReluActivateFunc()));
-        layers.push_back(new abcdl::fnn::FullConnLayer(32, 16, new abcdl::framework::ReluActivateFunc()));
-        layers.push_back(new abcdl::fnn::OutputLayer(16, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
-        fnn.set_layers(layers);
-    }
+    fnn.set_alpha(0.05);
+    fnn.set_epoch(1);
+    /*
+    std::vector<abcdl::fnn::Layer*> layers;
+    layers.push_back(new abcdl::fnn::InputLayer(784));
+    layers.push_back(new abcdl::fnn::FullConnLayer(784, 128, new abcdl::framework::ReluActivateFunc()));
+    layers.push_back(new abcdl::fnn::FullConnLayer(128, 128, new abcdl::framework::ReluActivateFunc()));
+    layers.push_back(new abcdl::fnn::FullConnLayer(128, 64, new abcdl::framework::ReluActivateFunc()));
+    layers.push_back(new abcdl::fnn::FullConnLayer(64, 32, new abcdl::framework::ReluActivateFunc()));
+    layers.push_back(new abcdl::fnn::FullConnLayer(32, 16, new abcdl::framework::ReluActivateFunc()));
+    layers.push_back(new abcdl::fnn::OutputLayer(16, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
+    fnn.set_layers(layers);
+    fnn.load_model(path);
+    */
+    std::vector<abcdl::fnn::Layer*> layers;
+    layers.push_back(new abcdl::fnn::InputLayer(784));
+    layers.push_back(new abcdl::fnn::FullConnLayer(784, 30, new abcdl::framework::SigmoidActivateFunc()));
+    layers.push_back(new abcdl::fnn::OutputLayer(30, 10, new abcdl::framework::SigmoidActivateFunc(), new abcdl::framework::CrossEntropyCost()));
+    fnn.set_layers(layers);
 
     const int train_size = -1;
     const int test_size = -1;
@@ -37,7 +43,6 @@ int main(int argc, char** argv){
     //abcdl::utils::FashionMnistReader<real> helper("data");
     abcdl::utils::MnistReader<real> helper("data");
     abcdl::algebra::Mat train_data;
-
     helper.read_train_image(&train_data, train_size);
     abcdl::algebra::Mat train_label;
     helper.read_train_vec_label(&train_label, train_size);
@@ -46,8 +51,10 @@ int main(int argc, char** argv){
     helper.read_test_image(&test_data, test_size);
     abcdl::algebra::Mat test_label;
     helper.read_test_vec_label(&test_label, test_size);
-
-    fnn.train(train_data, train_label, test_data, test_label);
-
-    fnn.write_model(path);
+    real loss = 0;
+    for(size_t i = 0; i < 100; i++){
+        fnn.train(train_data, train_label);
+        fnn.evaluate(test_data, test_label, &loss);
+        fnn.write_model(path);
+    }
 }
